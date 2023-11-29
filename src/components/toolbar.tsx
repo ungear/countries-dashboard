589739732bcaa1a3f251d9846bbc2cfe16a174fe
Mikhail Istomin <mikhail_istomin@epam.com>
2023-11-29 11:55:15 +0400

    add search

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,7 +1,12 @@
-export function Toolbar(){
+export function Toolbar({setSearchTerm}){
+  function onSearchFieldChanged(e){
+    const normalizedTerm = e.target.value.trim().toLowerCase();
+    setSearchTerm(normalizedTerm);
+
+  }
   return (
     <div>
-      toolbar
+      <input type="text" onChange={onSearchFieldChanged}/>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Toolbar } from '../components/toolbar'
 import { CountriesGrid } from '../components/countriesGrid'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const COUNTRIES = [
+  { name: 'Russia', capital: 'Moscow' },
+  { name: 'Georgia', capital: 'Tbilisi' },
+];
 
 export default function Home() {
-  const COUNTRIES = [
-    { name: 'Russia', capital: 'Moscow' },
-    { name: 'Georgia', capital: 'Tbilisi' },
-  ]
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const countriesToShow = COUNTRIES.filter(x => {
+    return Object.values(x).some(f => f.toLowerCase().includes(searchTerm));
+  })
   return (
     <section>
-      <Toolbar></Toolbar>
-      <CountriesGrid countries={COUNTRIES}></CountriesGrid>
+      <Toolbar setSearchTerm={setSearchTerm}></Toolbar>
+      <CountriesGrid countries={countriesToShow}></CountriesGrid>
     </section>
   )
 }
