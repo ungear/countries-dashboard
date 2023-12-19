6124d869c48987b8fe6475d08b19a9102bcfab0f
Mikhail Istomin <mikhail_istomin@epam.com>
2023-12-19 23:17:49 +0400

    add search button

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,12 +1,17 @@
-export function Toolbar({searchTerm, setSearchTerm}){
-  function handleChange(e){
-    const normalizedTerm = e.target.value.trim().toLowerCase();
-    setSearchTerm(normalizedTerm);
+import { useState } from 'react'
 
+export function Toolbar({searchTerm, setSearchTerm}){
+  const [term, setTerm] = useState(searchTerm);
+  function handleSearchClick(){
+    const normalizedTerm = term.trim().toLowerCase();
+    setSearchTerm(normalizedTerm);
+  }
+  function handeChangeTerm(e){
+    setTerm(e.target.value);
   }
   return (
     <div>
-      <input type="text" onChange={handleChange} value={searchTerm}/>
+      <input type="text" onChange={handeChangeTerm} value={term}/><button onClick={handleSearchClick}>Search</button>
     </div>
   )
 }
