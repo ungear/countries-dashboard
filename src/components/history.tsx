dd92c72699e57c92680309c381b8f49c5716f682
Mikhail Istomin <mikhail_istomin@epam.com>
2023-12-20 00:09:45 +0400

    apply reducer

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -2,7 +2,7 @@ export function History({searchHistory, onHistoryClick}){
   return (
     <ul>
       {searchHistory.map(h => (
-        <li key={h.id} onClick={() => onHistoryClick(h)}>{h.val}</li>
+        <li key={h.id} onClick={() => onHistoryClick(h)}>{h.value}</li>
       ))}
     </ul>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,34 @@ import styles from '@/styles/Home.module.css'
 import { Toolbar } from '../components/toolbar'
 import { History } from '../components/history'
 import { CountriesGrid } from '../components/countriesGrid'
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 
 const COUNTRIES = [
   { name: 'Russia', capital: 'Moscow' },
   { name: 'Georgia', capital: 'Tbilisi' },
 ];
-
+function historyReducer(history, action){
+  switch(action.type){
+    case 'added': {
+      const newCounter = history.counter + 1;
+      const newItem = {
+        value: action.value,
+        id: newCounter
+      };
+      return {
+        counter: newCounter,
+        items: [...history.items, newItem]
+      }
+    } 
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchHistory, setSearchHistory] = useState([]);
-  const [historyCounter, setHistoryCounter] = useState(0);
+  const [selectedId, setSelectedId] = useState(0);
+  const [history, dispatchHistory] = useReducer(historyReducer, { items: [], counter: 0});
 
   const countriesToShow = COUNTRIES.filter(x => {
     return Object.values(x).some(f => f.toLowerCase().includes(searchTerm));
@@ -25,22 +42,20 @@ export default function Home() {
     if(val.trim().length > 0) pushHistoryItem(val);
   }
   function onHistoryClick(historyItem){
-    setSearchTerm(historyItem.val);
+    setSearchTerm(historyItem.value);
+    setSelectedId(historyItem.id)
   }
-  function pushHistoryItem(val){
-    const newHistoryItem = {
-      val,
-      id: historyCounter
-    };
-    setHistoryCounter(historyCounter + 1);
-    const newHistory = [...searchHistory, newHistoryItem];
-    setSearchHistory(newHistory);
+  function pushHistoryItem(value){
+    dispatchHistory({
+      type: 'added',
+      value,
+    })
   }
   return (
     <section className={styles.main}>
-      <History searchHistory={searchHistory} onHistoryClick={onHistoryClick}></History>
+      <History searchHistory={history.items} onHistoryClick={onHistoryClick}></History>
       <div>
-        <Toolbar searchTerm={searchTerm} setSearchTerm={onSearchTermUpdate}></Toolbar>
+        <Toolbar searchTerm={searchTerm} setSearchTerm={onSearchTermUpdate} key={selectedId}></Toolbar>
         <CountriesGrid countries={countriesToShow}></CountriesGrid>
       </div>
     </section>
