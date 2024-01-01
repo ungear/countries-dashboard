bd94d517e5e312c25896b4f505a5137eb0914dd6
Mikhail Istomin <mikhail_istomin@epam.com>
2024-01-01 15:14:59 +0400

    add basic css

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -1,8 +1,13 @@
+import styles from './history.module.css';
+
 export function History({searchHistory, onHistoryClick}){
   return (
-    <ul>
+    <ul className={styles.history}>
       {searchHistory.map(h => (
-        <li key={h.id} onClick={() => onHistoryClick(h)}>{h.value} ({h.resultsNumber})</li>
+        <li key={h.id} className={styles.history__item}>
+          <span className={styles.history__term} onClick={() => onHistoryClick(h)}>{h.value}</span> 
+          ({h.resultsNumber})
+        </li>
       ))}
     </ul>
   )
