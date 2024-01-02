b91be4ec088f20bbd8f9c2543bfac396b9772cd0
Mikhail Istomin <mikhail_istomin@epam.com>
2024-01-02 13:09:24 +0400

    load countries from API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/Home.module.css'
 import { Toolbar } from '../components/toolbar'
 import { History } from '../components/history'
 import { CountriesGrid } from '../components/countriesGrid'
-import { useState, useReducer } from 'react'
+import { useState, useReducer, useEffect } from 'react'
 
 const COUNTRIES = [
   { name: 'Russia', capital: 'Moscow', continent: 'Eurasia', population: 143, currency: 'Ruble (₽)' },
@@ -31,16 +31,25 @@ function historyReducer(history, action){
   }
 }
 export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_ANALYTICS_ID)
   const [searchTerm, setSearchTerm] = useState('');
-  const [countriesToShow, setCountriesToShow] = useState(COUNTRIES);
+  const [countriesToShow, setCountriesToShow] = useState([]);
   const [history, dispatchHistory] = useReducer(historyReducer, { items: [], counter: 0});
   const columnsStartState = Object.keys(COUNTRIES[0]).map(x => ({ name: x, isActive: true}))
   const [columns, setColumns] = useState(columnsStartState);
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    if(countries.length === 0) {
+      fetch((process.env.NEXT_PUBLIC_ANALYTICS_ID as string))
+        .then(x => x.json())
+        .then(x => { setCountries(x); setCountriesToShow(x)})
+    }
+  },[countries])
 
   function applySearch(val, shouldAddToHistory = true){
     setSearchTerm(val);
-    const filteredCountries = COUNTRIES.filter(x => {
-      return Object.values(x).some(f => f.toString().toLowerCase().includes(val));
+    const filteredCountries = countries.filter(x => {
+      return Object.values(x).some((f:any) => f.toString().toLowerCase().includes(val));
     })
     setCountriesToShow(filteredCountries);
     if(shouldAddToHistory)
@@ -73,9 +82,14 @@ export default function Home() {
           reset={handleReset} 
           columns={columns}
           onColumnsChange={handleColumnsChange}></Toolbar>
-        <CountriesGrid 
-          columns={columns.filter(x => x.isActive)}
-          countries={countriesToShow}></CountriesGrid>
+        {(countries.length === 0) &&
+          <p> loading </p>
+        }
+        {(countries.length > 0) &&
+          <CountriesGrid 
+            columns={columns.filter(x => x.isActive)}
+            countries={countriesToShow}></CountriesGrid>
+        }
       </div>
     </section>
   )
